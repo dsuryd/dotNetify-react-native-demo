de123bcd2f5f3076f005420182fa8ee95570bd00
dsuryd <dsuryd@gmail.com>
2017-09-22 21:07:06 -0700

    Set token expiration to 2 mins.

--- server/AuthServer.cs
+++ server/AuthServer.cs
@@ -38,7 +38,7 @@ namespace server
       public string Path { get; set; } = "/token";
       public string Issuer { get; set; }
       public string Audience { get; set; }
-      public TimeSpan Expiration { get; set; } = TimeSpan.FromSeconds(30);
+      public TimeSpan Expiration { get; set; } = TimeSpan.FromSeconds(120);
       public SigningCredentials SigningCredentials { get; set; }
    }
 
